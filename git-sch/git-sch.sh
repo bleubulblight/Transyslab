e2cbd71aa6130ebfbab76bed954a56920e8ddfc6
BleuBulbLight <byulorg2@gmail.com>
2022-03-11 13:17:10 +0900

    commit 2022-03-.md

---
+++ git-sch/git-sch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Y=$(date +%Y)
+M=$(date +%m)
+D=$(data +%d)
+
+Ym=$Y-$M
+Ymd=$Y-$M-$D
+GitRep="Transyslab"
+HomeDir="/mnt/c/GitRepo"
+
+Gitdir="$HomeDir/$GitRep"
+FileDir="$Homedir/$GitRep/$Ym"
+FileNm="$Ymd".md
+
+mkdir -p $FileDir
+
+echo "### $Ymd Diary" >> $FileDir/$FileNm
+
+cd $Gitdir
+git add .
+git commit -m "commit $FileNm"
+git push origin master
